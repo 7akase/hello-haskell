bb7ad8599c0ac0e94e9e53d7e0ed60f673694175
7akase <yasuhide.takase@gmail.com>
2016-03-20 02:40:24 +0900

    temp

--- fa.hs
+++ fa.hs
@@ -1,43 +1,42 @@
 import Control.Applicative
+import Data.Monoid
+import qualified Data.Foldable as F
 
-data Net a
-  = FAC (Net a) (Net a) (Net a) -- Full Adder Carry
-  | FAS (Net a) (Net a) (Net a) -- Full Adder Sum
-  | HAC (Net a) (Net a)          -- Half Adder Carry
-  | HAS (Net a) (Net a)          -- Half Adder Sum
-  | NET a                          -- Net
-  deriving (Show)
+data Inst a
+  = Inst {cell :: String, input :: [Inst a], name :: Maybe a} deriving (Show)
+{-
+instance (Show a, Eq a) => Show (Inst a) where
+  show x | name x == Nothing = cell x
+         | otherwise         = cell x ++ show (Just (name x))
+-}
 
-type ThisBit a = [Net a]
-type PrevBit a = [Net a]
+instance F.Foldable Inst where
+  foldMap f x | length (input x) == 0 = mempty 
+              | otherwise             = F.foldMap mappend (F.foldMap f (input x))
 
-netsToAdders :: [Net a] -> [Net a]
+type Bit a = [Inst a]
+type ThisBit a = Bit a
+type PrevBit a = Bit a
+
+netsToAdders :: [Inst a] -> [Inst a]
 netsToAdders []            = []
 netsToAdders [n1]          = [n1]
-netsToAdders [n1,n2]       = [HAS n1 n2]
-netsToAdders (n1:n2:n3:ns) = [FAS n1 n2 n3] ++ netsToAdders ns
+netsToAdders [n1,n2]       = [(Inst "HAS" [n1,n2]    Nothing)]
+netsToAdders (n1:n2:n3:ns) = [(Inst "FAS" [n1,n2,n3] Nothing)] ++ netsToAdders ns
 
-isFA :: Net a -> Bool
-isFA (FAS _ _ _) = True
-isFA _           = False
+sumToCarry :: Inst a -> Inst a
+sumToCarry x@(Inst {cell = cell, name = name, input = xs })
+  | cell == "FAS" || cell == "HAS" = x {cell = "FAC"} 
 
-isHA :: Net a -> Bool
-isHA (HAS _ _) = True
-isHA _         = False
+isAdder :: Inst a -> Bool
+isAdder Inst {cell = cell} | cell == "FAS" || cell == "HAS" = True
+                           | otherwise = False
 
-isNET :: Net a -> Bool
-isNET (NET _) = True
-isNet _       = False
+-- >>> wallace (NET <$> [1..2]) $ wallace (NET <$> [3..11]) []
+wallace :: ThisBit a -> PrevBit a -> [Inst a]
+wallace lsb [] | length lsb >= 3 = wallace (netsToAdders lsb) []
+               | otherwise       = lsb 
+wallace msb lsb = netsToAdders (msb ++ (sumToCarry <$> (filter isAdder)  lsb))
 
-isAdder :: Net a -> Bool
-isAdder x = isFA x || isHA x
-
-sumToCarry :: Net a -> Net a
-sumToCarry (FAS n1 n2 n3) = FAC n1 n2 n3
-sumToCarry (HAS n1 n2)    = HAC n1 n2
-
--- >>> wallace (NET <$> [1..2] $ wallace (NET <$> [3..11]) []
-wallace :: ThisBit a -> PrevBit a -> [Net a]
-wallace as [] | length as >= 3 = wallace (netsToAdders as) []
-              | otherwise      = as
-wallace as bs = netsToAdders (as ++ (sumToCarry <$> (filter isAdder)  bs))
+a = replicate 3 $ Inst "pin" [] Nothing
+b = replicate 8 $ Inst "pin" [] Nothing
