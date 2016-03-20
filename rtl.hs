f02a4400ce8e2d7abe8cd0c6aa1a7805f841a0ae
7akase <yasuhide.takase@gmail.com>
2016-03-20 23:53:44 +0900

    add genNodeName

--- rtl.hs
+++ rtl.hs
@@ -1,10 +1,23 @@
 import Control.Applicative
+import Control.Monad.State
 import Data.Monoid
 import Data.Maybe
 import Data.Tree
 import qualified TertiaryTree as T
 
--- data Inst a = Inst {cell :: String, name :: a}
+data Inst a = Inst {cell :: String, name :: a}
+
+nodePrefix :: String
+nodePrefix = "N"
+
+genNodeName :: State Int String 
+genNodeName = state (\s -> (nodePrefix ++ show s, s + 1))
+
+test0 :: State Int String 
+test0 = do
+  genNodeName
+  genNodeName
+
 
 nums = Just <$> [1..25]
 numTree = T.foldr T.treeInsert (T.singleton (Just 4)) nums
