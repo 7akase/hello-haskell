daef2b0a7c7ffc4ea0750de3743fd92196945eda
7akase <yasuhide.takase@gmail.com>
2016-03-23 10:01:02 +0900

    add numberTree

--- TertiaryTree.hs
+++ TertiaryTree.hs
@@ -1,49 +1,48 @@
 module TertiaryTree(
-  tertiarize, treeInsert, singleton,
-  fmap, fmapT, foldrT, F.foldl, F.foldr,
-  showT) where
+  insertTree, singleton,
+  fmapTree, foldrTree, zipTree,
+  showTree, numberTree) where
+
+import Data.Tree
 
 import Control.Applicative
-import Data.Tree
 import Data.Maybe
 import Data.Monoid
+import Data.Traversable
 import qualified Data.Foldable as F
 
-tertiarize :: Tree (Maybe a) -> Tree (Maybe a)
-tertiarize (Node r ts0) = (Node r (newForest ts0))
-  where
-    newForest [] = [] -- Leaf
-    newForest [t] = [tertiarize t]
-    newForest [t1,t2] = fmap tertiarize [t1,t2]
-    newForest [t1,t2,t3] = fmap tertiarize [t1,t2,t3]
-    newForest (t1:t2:t3:ts) = newForest ((tertiarize (Node Nothing [t1,t2,t3])):ts) 
+{-#LANGUAGE DeriveTraversable #-}
+
 
 singleton :: a -> Tree a
 singleton x = Node x []
 
-treeInsert :: (Ord a) => a -> Tree a -> Tree a
-treeInsert x (Node r ts)
+insertTree :: (Ord a) => a -> Tree a -> Tree a
+insertTree x (Node r ts)
   | x == r = Node x ts
   | x <  r = Node r ((singleton x):ts)
   | x >  r = Node x (ts ++ [(singleton r)])
 
-fmapT :: (Tree a -> Tree b) -> Tree a -> Tree b
-fmapT f t@(Node x ts) = Node x' (map (fmapT f) ts)
+fmapTree :: (Tree a -> Tree b) -> Tree a -> Tree b
+fmapTree f t@(Node x ts) = Node x' (map (fmapTree f) ts)
   where (Node x' ts') = f t 
 
-foldrT :: Monoid m => (Tree a -> m -> m) -> m -> Tree a -> m
-foldrT f z t@(Node r []) = f t z
-foldrT f z t@(Node r ts) = (f t mempty) `mappend` (foldr mappend mempty (fmap (foldrT f mempty) ts))
+foldrTree :: Monoid m => (Tree a -> m -> m) -> m -> Tree a -> m
+foldrTree f z t@(Node r []) = f t z
+foldrTree f z t@(Node r ts) = (f t mempty) `mappend` (foldr mappend mempty (fmap (foldrTree f mempty) ts))
 
-showT :: (Show a) => Tree a -> String
-showT = drawTree . (fmap show)
+zipTree :: Tree a -> Tree b -> Tree (a,b)
+zipTree (Node r1 xs1) (Node r2 xs2) = Node (r1,r2) (zipTree <$> xs1 <*> xs2) 
 
+showTree :: (Show a) => Tree a -> String
+showTree = drawTree . (fmap show)
+
+numberTree :: Tree a -> Tree (Integer, a)
+numberTree = snd . mapAccumL (\acc x -> (acc + 1, (acc + 1, x))) 0
 
 nums = Just <$> [1..20]
-numTree = F.foldr treeInsert (singleton (Just 4)) nums
+numTree = F.foldr insertTree (singleton (Just 4)) nums
 
 main = do
   putStrLn $ drawTree (fmap show numTree)
-test = do
-  putStrLn . drawTree . (fmap show) . tertiarize $ numTree
 
--- rtl.hs
+++ rtl.hs
@@ -2,7 +2,7 @@ import Control.Applicative
 import Data.Monoid
 import Data.Maybe
 import Data.Tree
-import qualified TertiaryTree as T
+import TertiaryTree
 
 data Inst a = Inst {cell :: String, name :: Maybe a, from :: [Maybe a]}
  deriving (Show)
@@ -10,14 +10,14 @@ data Inst a = Inst {cell :: String, name :: Maybe a, from :: [Maybe a]}
 toInst :: Tree (Maybe a) -> Tree (Maybe (Inst a))
 toInst (Node Nothing  ts) = Node (Just (Inst "" Nothing 
                                              (fmap (\(Node r _) -> r) ts)))
-                                 (fmap (T.fmapT toInst) ts)
+                                 (fmap (fmapTree toInst) ts)
 toInst (Node (Just x) ts) = Node (Just (Inst "" (Just x)
                                              (fmap (\(Node r _) -> r) ts)))
-                                 (fmap (T.fmapT toInst) ts)
+                                 (fmap (fmapTree toInst) ts)
  
--- usage : putStrLn . T.showT . convert $ T.tertiarize numTree
+-- usage : putStrLn . showTree . convert $ tertiarize numTree
 convert :: Tree (Maybe a) -> Tree (Maybe (Inst a))
-convert = T.fmapT toInst 
+convert = fmapTree toInst 
 
 -- auxial functions for RTL generator
 
@@ -26,13 +26,25 @@ isAdder (Node _ ts) | length ts == 3 = True
                     | otherwise      = False
 
 carry :: Tree (Maybe (Inst a)) -> [Tree (Maybe (Inst a))]
-carry t = T.foldrT f [] t
+carry t = foldrTree f [] t
   where f x = if isAdder x then (x{subForest = []}:) else id
 
+giveNumber :: [b] -> Tree a -> Tree (b,a)
+giveNumber = undefined
+-- giveNumber (n:ns) t@(r ts) = Node (n,r) giveNumber 
+
+tertiarize :: Tree (Maybe a) -> Tree (Maybe a)
+tertiarize (Node r ts0) = (Node r (newForest ts0))
+  where
+    newForest [] = [] -- Leaf
+    newForest [t] = [tertiarize t]
+    newForest [t1,t2] = fmap tertiarize [t1,t2]
+    newForest [t1,t2,t3] = fmap tertiarize [t1,t2,t3]
+    newForest (t1:t2:t3:ts) = newForest ((tertiarize (Node Nothing [t1,t2,t3])):ts) 
 
 wallace :: [Tree (Maybe (Inst a))] -> [Tree (Maybe (Inst a))]
-wallace (x:[]) = [T.tertiarize x]
-wallace (x:xs) = [T.tertiarize (Node Nothing (x:(carry (head (wallace xs)))))] ++ wallace xs
+wallace (x:[]) = [tertiarize x]
+wallace (x:xs) = [tertiarize (Node Nothing (x:(carry (head (wallace xs)))))] ++ wallace xs
 
 -- *****************************************************************************
 --  configure
@@ -44,19 +56,17 @@ nodePrefix = "N"
 -- *****************************************************************************
 
 nums = Just <$> [1..25]
-numTree = T.foldr T.treeInsert (T.singleton (Just 4)) nums
+numTree = foldr insertTree (singleton (Just 4)) nums
 
 pins = Just <$> [1..4]
-pinTree = T.foldr T.treeInsert (T.singleton (Just 100)) pins
+pinTree = foldr insertTree (singleton (Just 100)) pins
 
 test0 = do 
-  putStrLn . drawTree $ fmap (show . maybeToList) ((\t -> do fmap (\x -> if x == Nothing then Just 0 else x) t) (T.tertiarize pinTree))
+  putStrLn . drawTree $ fmap (show . maybeToList) ((\t -> do fmap (\x -> if x == Nothing then Just 0 else x) t) (tertiarize pinTree))
 
 test1 = do
-  putStrLn . drawTree $ fmap (show . maybeToList) (T.tertiarize numTree)
-test2 = do
-  T.foldr (+) 0 (fmap (head . (1:) . maybeToList) numTree)
+  putStrLn . drawTree $ fmap (show . maybeToList) (tertiarize numTree)
 test3 = do
-  foldr (++) "" (fmap (\x -> show (maybeToList (rootLabel x))) (subForest (T.tertiarize numTree)))
+  foldr (++) "" (fmap (\x -> show (maybeToList (rootLabel x))) (subForest (tertiarize numTree)))
 
 
